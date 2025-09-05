db82bdc14d54806e591e6806f672631ad48dbcc1
Busra Mestan <bmestan50@gmail.com>
2025-09-05 15:30:52 +0300

    [SP-1] - feat: Create ProductService Interface

---
+++ src/main/java/com/busramestan/springboot/entity/Product.java
@@ -0,0 +1,26 @@
+package com.busramestan.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+}
